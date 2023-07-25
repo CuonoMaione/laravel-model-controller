87748691fd94fa67246836dd0d7afd731c773c01
CuonoMaione <127331845+CuonoMaione@users.noreply.github.com>
2023-07-26 01:56:19 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('main-content')
+
+    <h1>Helloword!</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 btn btn-danger">
+                    ciao
+                </div>
+            </div>
+        </div>
+    
+
+@endsection
